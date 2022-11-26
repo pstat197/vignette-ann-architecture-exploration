6cbb687aaf5eccd8407272858f7c7b024204f4be
SammySuliman <96219622+SammySuliman@users.noreply.github.com>
2022-11-26 00:00:17 -0800

    got nn to converge

---
+++ neural_network.R
@@ -0,0 +1,54 @@
+# Import Required packages
+#install.packages("AppliedPredictiveModeling")
+library(neuralnet)
+library(tidyverse)
+library(tidymodels)
+library(keras)
+library(tensorflow)
+library(MASS)
+library(AppliedPredictiveModeling)
+# abalone dataset
+data(abalone)
+
+# Normalize the data
+maxs <- apply(abalone[2:8], 2, max) 
+mins <- apply(abalone[2:8], 2, min)
+scaled <- as.data.frame(scale(abalone[2:8], center = mins, 
+                              scale = maxs - mins))
+abalone['age'] <- abalone['Rings'] + 1.5
+age <- abalone['age']
+
+# Split the data into training and testing set
+set.seed(12312001)
+partitions <- scaled %>% initial_split(prop = 0.8)
+partitions2 <- age %>% initial_split(prop = 0.8)
+test_ <- testing(partitions)
+train_ <- training(partitions)
+train_age <- training(partitions2)
+test_age <- testing(partitions2)
+train2 <- cbind(train_, train_age)
+test2 <- cbind(test_, test_age)
+# Build Neural Network
+nn <- neuralnet(age ~ LongestShell + Diameter + 
+                  Height + WholeWeight + ShuckedWeight + VisceraWeight + 
+                  ShellWeight, data = train2, hidden = c(2, 2), 
+                linear.output = TRUE, stepmax = 50000, learningrate = 500)
+
+# Predict on test data
+pr.nn <- neuralnet::compute(nn, test2)
+
+# Compute mean squared error
+pr.nn_ <- pr.nn$net.result * (max(age) - min(age)) 
++ min(age)
+test.r <- (test2$age) * (max(test2$age) - min(test2$age)) + 
+  min(test2$age)
+MSE.nn <- sum((test.r - pr.nn_)^2) / nrow(test_)
+
+# Plot the neural network
+plot(nn)
+
+# Plot regression line
+plot(test2$age, pr.nn_, col = "red", 
+     main = 'Real vs Predicted')
+abline(0, 1, lwd = 2)
+
--- sample_code.R
+++ sample_code.R
@@ -1,41 +1,31 @@
 # Import Required packages
-#install.packages("AppliedPredictiveModeling")
+set.seed(500)
 library(neuralnet)
-library(tidyverse)
-library(tidymodels)
-library(keras)
-library(tensorflow)
 library(MASS)
-library(AppliedPredictiveModeling)
-# abalone dataset
-data(abalone)
 
+# Boston dataset from MASS
+data <- Boston
 # Normalize the data
-maxs <- apply(abalone[2:8], 2, max) 
-mins <- apply(abalone[2:8], 2, min)
-scaled <- as.data.frame(scale(abalone[2:8], center = mins, 
+maxs <- apply(data, 2, max) 
+mins <- apply(data, 2, min)
+scaled <- as.data.frame(scale(data, center = mins, 
                               scale = maxs - mins))
-abalone['age'] <- abalone['Rings'] + 1.5
-age <- abalone['age']
 
 # Split the data into training and testing set
-set.seed(12312001)
-partitions <- scaled %>% initial_split(prop = 0.8)
-partitions2 <- age %>% initial_split(prop = 0.8)
-test_ <- testing(partitions)
-train_ <- training(partitions)
-train_age <- training(partitions2)
-test_age <- testing(partitions2)
-train2 <- cbind(train_, train_age)
-test2 <- cbind(test_, test_age)
+index <- sample(1:nrow(data), round(0.75 * nrow(data)))
+train_ <- scaled[index,]
+test_ <- scaled[-index,]
+
+
 # Build Neural Network
-nn <- neuralnet(age ~ LongestShell + Diameter + 
-                  Height + WholeWeight + ShuckedWeight + VisceraWeight + 
-                  ShellWeight, data = train2, hidden = c(5, 3), 
+nn <- neuralnet(medv ~ crim + zn + indus + chas + nox 
+                + rm + age + dis + rad + tax + 
+                  ptratio + black + lstat, 
+                data = train_, hidden = c(5, 3), 
                 linear.output = TRUE)
 
 # Predict on test data
-pr.nn <- neuralnet::compute(nn, test_)
+pr.nn <- compute(nn, test_[,1:13])
 
 # Compute mean squared error
 pr.nn_ <- pr.nn$net.result * (max(data$medv) - min(data$medv)) 
@@ -46,9 +36,3 @@ MSE.nn <- sum((test.r - pr.nn_)^2) / nrow(test_)
 
 # Plot the neural network
 plot(nn)
-
-# Plot regression line
-plot(test$medv, pr.nn_, col = "red", 
-     main = 'Real vs Predicted')
-abline(0, 1, lwd = 2)
-
