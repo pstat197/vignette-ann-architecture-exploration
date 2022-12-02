c2237f15f2a15a10c2a7f6f3cd53b05e74f5e006
ramirezgis <ramirezgis1225@gmail.com>
2022-12-01 16:52:59 -0800

    normalized data and made simple neural network

--- scripts/code_delete.R
+++ scripts/code_delete.R
@@ -8,24 +8,77 @@ library(AppliedPredictiveModeling)
 data(abalone)
 abalone['age'] <- abalone['Rings'] + 1.5
 
-#apply class
-sapply(abalone, class)
-head(abalone)
+#normalize (min and max to each individual variable)
+abalone_mine <- abalone #made a copy()
+abalone_mine$LongestShell <- (abalone_mine$LongestShell - 
+                                min(abalone_mine$LongestShell)) / 
+  (max(abalone_mine$LongestShell) - min(abalone_mine$LongestShell))
+abalone_mine$Diameter <- (abalone_mine$Diameter - 
+                                min(abalone_mine$Diameter)) / 
+  (max(abalone_mine$Diameter) - min(abalone_mine$Diameter))
+abalone_mine$Height <- (abalone_mine$Height - 
+                                min(abalone_mine$Height)) / 
+  (max(abalone_mine$Height) - min(abalone_mine$Height))
+abalone_mine$WholeWeight <- (abalone_mine$WholeWeight - 
+                                min(abalone_mine$WholeWeight)) / 
+  (max(abalone_mine$WholeWeight) - min(abalone_mine$WholeWeight))
+abalone_mine$ShuckedWeight <- (abalone_mine$ShuckedWeight - 
+                                min(abalone_mine$ShuckedWeight)) / 
+  (max(abalone_mine$ShuckedWeight) - min(abalone_mine$ShuckedWeight))
+abalone_mine$VisceraWeight <- (abalone_mine$VisceraWeight - 
+                                min(abalone_mine$VisceraWeight)) / 
+  (max(abalone_mine$VisceraWeight) - min(abalone_mine$VisceraWeight))
+abalone_mine$ShellWeight <- (abalone_mine$ShellWeight - 
+                                min(abalone_mine$ShellWeight)) / 
+  (max(abalone_mine$ShellWeight) - min(abalone_mine$ShellWeight))
+abalone_mine$Rings <- (abalone_mine$Rings - min(abalone_mine$Rings)) / 
+  (max(abalone_mine$Rings) - min(abalone_mine$Rings))
+abalone_mine$age <- (abalone_mine$age - min(abalone_mine$age)) / 
+  (max(abalone_mine$age) - min(abalone_mine$age))
+
+#factor variables to dummy variables: Type
+table(abalone_mine$Type)
+levels(abalone_mine$Type)
+head(model.matrix(~Type, data = abalone_mine))
+abalone_mine$Type <- relevel(abalone_mine$Type, ref = "I")
+head(model.matrix(~Type, data = abalone_mine))
+abalone_matrix <- model.matrix(~Type+LongestShell+Diameter+Height+
+                                 WholeWeight+ShuckedWeight+
+                                 VisceraWeight+ShellWeight+age, 
+                               data = abalone_mine)
+colnames(abalone_matrix)
+col_list <- paste(c(colnames(abalone_matrix[, -
+                                              c(1, 11)])), collapse = "+")
+col_list <- paste(c("age~", col_list), collapse="")
+f <- formula(col_list)
+
+#one hidden node
+library(neuralnet)
+set.seed(1234)
+nmodel1 <- neuralnet(f, data = abalone_matrix, hidden = 1, 
+                    threshold = 0.01, 
+                    learningrate.limit = NULL, 
+                    learningrate.factor = list(minus = 0.5, plus = 1.2),
+                    algorithm = "rprop+")
+
+# Predict on test data
+pr.nn2 <- neuralnet::compute(nmodel, abalone_matrix)
+
+# Compute mean squared error
+pr.nn_2 <- pr.nn2$net.result * (max(age) - min(age)) 
++ min(age)
+test.r2 <- (abalone_matrix$age) * (max(abalone_matrix$age) 
+                                   - min(abalone_matrix$age)) + 
+  min(abalone_matrix$age)
+MSE.nn <- sum((test.r - pr.nn_)^2) / nrow(test_)
+
+# Plot the neural network
+plot(nmodel1)
 
 #normalize (min-max 0-1)
 #abalone$age = (abalone$)
 
 
-
-# install.packages("AppliedPredictiveModeling")
-library(neuralnet)
-library(tidyverse)
-library(tidymodels)
-library(AppliedPredictiveModeling)
-# abalone dataset
-data(abalone)
-
-##binary classification (student mental health)
 
 #change factor???
 abalone_mine <- abalone
