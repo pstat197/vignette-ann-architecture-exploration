a44d0bd591364515a0c40ac35adc10fbae315a79
Alexis Navarra <alexis.c.navarra@gmail.com>
2022-12-07 16:17:07 -0800

    added epochs exploration

--- scripts/nn_epochs_val.R
+++ scripts/nn_epochs_val.R
@@ -18,8 +18,8 @@ x_test <- test_all %>% # test_
 y_test <- test_all$age %>% # test_age
   as.matrix()
 
+# neural network - 100 epochs
 model <- keras_model_sequential(input_shape = 7)
-
 model <- keras_model_sequential(input_shape = ncol(x_train)) %>%
   layer_dense(10) %>%
   layer_dense(1) %>%
@@ -53,3 +53,76 @@ evaluate(model, x_test, y_test)
 
 write_rds(model, file = "models/nn_100_epochs.rds")
 nn_100_epochs <- read_rds(file = "models/nn_100_epochs.rds")
+
+
+## neural network - 50 epochs
+model2 <- keras_model_sequential(input_shape = 7)
+model2 <- keras_model_sequential(input_shape = ncol(x_train)) %>%
+  layer_dense(10) %>%
+  layer_dense(1) %>%
+  layer_dense(100) %>%
+  layer_dense(1) %>%
+  layer_dense(10) %>%
+  layer_activation(activation = 'relu')
+
+
+model2 %>%
+  compile(
+    loss = 'MeanAbsoluteError',
+    optimizer = 'adam',
+    metrics = 'MeanSquaredError'
+  )
+
+history <- model2 %>%
+  fit(x = x_train, 
+      y = y_train,
+      epochs = 50,
+      validation_split = 0.3)
+
+summary(model2)
+plot(history)
+
+# retrieve weights
+get_weights(model2)
+
+# evaluate on specified data
+evaluate(model2, x_test, y_test)
+
+write_rds(model2, file = "models/nn_50_epochs.rds")
+nn_50_epochs <- read_rds(file = "models/nn_50_epochs.rds")
+
+## neural network - 10 epochs
+model3 <- keras_model_sequential(input_shape = 7)
+model3 <- keras_model_sequential(input_shape = ncol(x_train)) %>%
+  layer_dense(10) %>%
+  layer_dense(1) %>%
+  layer_dense(100) %>%
+  layer_dense(1) %>%
+  layer_dense(10) %>%
+  layer_activation(activation = 'relu')
+
+
+model3 %>%
+  compile(
+    loss = 'MeanAbsoluteError',
+    optimizer = 'adam',
+    metrics = 'MeanSquaredError'
+  )
+
+history <- model3 %>%
+  fit(x = x_train, 
+      y = y_train,
+      epochs = 10,
+      validation_split = 0.3)
+
+summary(model3)
+plot(history)
+
+# retrieve weights
+get_weights(model3)
+
+# evaluate on specified data
+evaluate(model3, x_test, y_test)
+
+write_rds(model3, file = "models/nn_10_epochs.rds")
+nn_10_epochs <- read_rds(file = "models/nn_10_epochs.rds")
